73d6db2ec95261a9a22a617d4afee4f01e7acfbd
tartaruga <nefomkin@gmail.com>
2021-04-08 21:33:03 +0300

    add component Media

---
+++ src/components/common/Media.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react';
+
+const Media = ({ query, children }) => {
+  const media: MediaQueryList = window.matchMedia(query);
+  const [matches, setMatches] = useState(media.matches);
+
+  useEffect(() => {
+    media.addListener(update);
+
+    return () => {
+      media.removeListener(update);
+    };
+  });
+
+  function update({ matches }) {
+    setMatches(matches);
+  }
+
+  return children(matches);
+};
+
+export default Media;
